ddc5626997ec1aa101946822b99ce27d6bfed662
Asma Liaquat <asma.liaquat@systemsltd.com>
2022-05-13 14:19:42 +0500

    karma and travis config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-app app is running!');
-  });
 });
